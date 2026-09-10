7a868af523fbec1818a365e00027925a30e8ee55
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-10 04:50:07 +0300

    Q1

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+
+namespace c_05
+{
+    class Book
+    {
+        // Question 1
+        private string password = "secret";
+
+        // Question 2
+        internal int copiesInStock = 5;
+
+        // Question 3
+        public string Title;
+
+        // Question 4
+        public Genre Genre { get; set; }
+    }
+
+    // Question 4
+    enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Book book = new Book();
+
+
+
+            #region Question 2 - internal
+            Console.WriteLine(book.copiesInStock);
+            #endregion
+
+        }
+    }
+}
